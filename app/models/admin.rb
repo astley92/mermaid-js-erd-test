c0e52e449448dff9bb71268e4546dba3f3f849ea
Blake Astley <astley92@hotmail.com>
2024-06-05 16:59:03 +1000

    Create admins

---
+++ app/models/admin.rb
@@ -0,0 +1,12 @@
+# == Schema Information
+#
+# Table name: admins
+#
+#  id         :bigint           not null, primary key
+#  email      :string
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class Admin < ApplicationRecord
+end
